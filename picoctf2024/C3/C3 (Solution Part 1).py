519cac3a48631e17d39361e5a9e02dc79a4fc479
Alex Zhu <alexyifanzhu@gmail.com>
2024-04-01 11:18:02 -0400

    C3 solution

---
+++ picoctf2024/C3/C3 (Solution Part 1).py
@@ -0,0 +1,15 @@
+c="DLSeGAGDgBNJDQJDCFSFnRBIDjgHoDFCFtHDgJpiHtGDmMAQFnRBJKkBAsTMrsPSDDnEFCFtIbEDtDCIbFCFtHTJDKerFldbFObFCFtLBFkBAAAPFnRBJGEkerFlcPgKkImHnIlATJDKbTbFOkdNnsgbnJRMFnRBNAFkBAAAbrcbTKAkOgFpOgFpOpkBAAAAAAAiClFGIPFnRBaKliCgClFGtIBAAAAAAAOgGEkImHnIl"
+
+lookup1 = "\n \"#()*+/1:=[]abcdefghijklmnopqrstuvwxyz"
+lookup2 = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst"
+
+
+f=""
+prev=0
+for char in c:
+    ind=lookup2.index(char)
+    org=lookup1[(ind+prev)%40]
+    prev=(ind+prev)%40
+    f=f+org
+print(f)
+
